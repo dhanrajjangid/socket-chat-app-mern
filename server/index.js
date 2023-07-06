d03ac8db936e9db959e4890b78a95f4657f8fd1d
dhanraj jangid <dhanrajjangidst@gmail.com>
2023-07-06 15:26:20 +0530

    sample

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "https://perchatroom.vercel.app",
+    origin: "https://perchatroom.vercel.app/",
     credentials: true,
   },
 });
