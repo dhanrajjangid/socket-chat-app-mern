c1932de8182fe70dadef035a978418c32e75eaa9
dhanraj jangid <dhanrajjangidst@gmail.com>
2023-07-06 14:01:46 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express(); 
-const cors  = require("cors");
+const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
@@ -8,43 +8,46 @@ const messageRoute = require("./routes/messagesRoute");
 const socket = require("socket.io");
 
 dotenv.config();
-app.use(cors());
+app.use(cors({ origin: "https://perchatroom.vercel.app/" }));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
 app.use("/api/message", messageRoute);
 
 //mongoose connection
-mongoose.connect(process.env.MONGO_URL, {
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-    }).then(() => {
-        console.log("DB Connection Successful!")
-    }).catch((err) => console.log(err));
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connection Successful!");
+  })
+  .catch((err) => console.log(err));
 
- const server = app.listen(process.env.PORT, ()=>{
-    console.log(`Server started on Port ${process.env.PORT}`);
+const server = app.listen(process.env.PORT, () => {
+  console.log(`Server started on Port ${process.env.PORT}`);
 });
 
-const io = socket(server,{
-    cors: {
-        origin: "http://localhost:3000",
-        credentials: true,
-    },
+const io = socket(server, {
+  cors: {
+    origin: "https://perchatroom.vercel.app/",
+    credentials: true,
+  },
 });
 //store all online users inside this map
-global.onlineUsers =  new Map();
- 
-io.on("connection",(socket)=>{
-    global.chatSocket = socket;
-    socket.on("add-user",(userId)=>{
-        onlineUsers.set(userId,socket.id);
-    });
+global.onlineUsers = new Map();
 
-    socket.on("send-msg",(data)=>{
-        const sendUserSocket = onlineUsers.get(data.to);
-        if(sendUserSocket) {
-            socket.to(sendUserSocket).emit("msg-recieved",data.message);
-        }
-    });
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieved", data.message);
+    }
+  });
 });
