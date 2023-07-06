479b777fa3eaf231260a6bfd54d2f8b279cc5bee
dhanraj jangid <dhanrajjangidst@gmail.com>
2023-07-06 15:15:14 +0530

    changed url for api

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "https://socket-chat-app-mern.vercel.app";
+export const host = "https://mern-socket-chat-server.vercel.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
