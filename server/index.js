a99dbe333f025d392828424066f469c5141d6cd8
dhanraj jangid <dhanrajjangidst@gmail.com>
2023-07-06 15:07:00 +0530

    cors error resolved

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const messageRoute = require("./routes/messagesRoute");
 const socket = require("socket.io");
 
 dotenv.config();
-app.use(cors());
+app.use(cors({ origin: "https://perchatroom.vercel.app" }));
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
